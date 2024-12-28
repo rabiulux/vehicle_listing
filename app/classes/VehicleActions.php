0382f111a03972f6907f3ab0690bc2631e05200b
rabiulux <rabiulux@gmail.com>
2024-12-28 17:25:49 +0600

    New Line

---
+++ app/classes/VehicleActions.php
@@ -0,0 +1,8 @@
+<?php
+
+Interface VehicleActions{
+    public function addVehicle($data);
+    public function editVehicle($id, $data);
+    public function deleteVehicle($id);
+    public function getVehicles();
+}
